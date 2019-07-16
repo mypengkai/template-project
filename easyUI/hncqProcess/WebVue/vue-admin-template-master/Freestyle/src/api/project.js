30290295c62af812a237dcc0b688336d40ba11a8
liangtx <liangtx@9aff1686-bcb2-c349-8ec9-f59386159562>
2019-07-16 02:09:47 +0000

    更新接口

--- easyUI/hncqProcess/WebVue/vue-admin-template-master/Freestyle/src/api/project.js
+++ easyUI/hncqProcess/WebVue/vue-admin-template-master/Freestyle/src/api/project.js
@@ -1,26 +1,26 @@
 import request from '@/utils/request'
 
 export default {
-    // 新增
-    projectAdd(data) {
-        return request.post('/rest/projectItemInfo/projectadd', data)
-    },
-    // 列表 
-    projectList(data) {
-        return request.post('/rest/projectItemInfo/getList', data)
-    },
-    // 删除
-    projectDelete(id) {
-        return request.get(`/rest/projectItemInfo/delete/${id}`)
-    },
-    // 查询
-    projectCheck(id) {
-        return request.post(`/rest/projectItemInfo/chakan/${id}`)
-    },
-    // 批量导入
-    projectaddbyList(id) {
-        return request.post(`/rest/projectItemInfo/addbyList/${id}`)
-    }
+  // 新增
+  projectAdd(data) {
+    return request.post('/rest/projectItemInfo/projectadd', data)
+  },
+  // 列表
+  projectList(data) {
+    return request.post('/rest/projectItemInfo/getList', data)
+  },
+  // 删除
+  projectDelete(id) {
+    return request.get(`/rest/projectItemInfo/delete/${id}`)
+  },
+  // 查询
+  projectCheck(id) {
+    return request.post(`/rest/projectItemInfo/getProjectItemById/${id}`)
+  },
+  // 批量导入
+  projectaddbyList(id) {
+    return request.post(`/rest/projectItemInfo/addbyList/${id}`)
+  }
 }
 
 // export function getUser(params) {
