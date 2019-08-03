61ff2bc695f7975ee3f96a3919e2ab771dce9748
xux <xux@9aff1686-bcb2-c349-8ec9-f59386159562>
2019-08-03 06:48:08 +0000

    菜单问题

--- views/system/resource.vue
+++ views/system/resource.vue
@@ -11,30 +11,37 @@
         </el-col>
       </el-row>
     </div>
-    <el-table class="textList" :data="menuList" style="width: 100%" height="68vh" row-key="id" border
-              :default-expand-all="false" :tree-props="{children: 'children', hasChildren: 'hasChildren'}">
-      <el-table-column prop="meta.title" label="菜单标题" align="center"></el-table-column>
-      <el-table-column prop="name" label="菜单名称" align="center"></el-table-column>
-      <el-table-column prop="component" label="组件名称" align="center"></el-table-column>
-      <el-table-column prop="path" label="菜单地址" align="center"></el-table-column>
-      <el-table-column label="类型">
-        <template slot-scope="scope">{{ scope.row.type==='0' ? '按钮': '菜单' }}
-        </template>
+    <tree-table :data="menuList" class="textList" border row-key>
+      <el-table-column label="菜单标题" height="250" align="center">
+        <template slot-scope="scope">{{ scope.row.meta.title }}</template>
       </el-table-column>
-      <el-table-column label="操作" align="center">
+      <el-table-column label="菜单名称" height="250" align="center">
+        <template slot-scope="scope">{{ scope.row.name }}</template>
+      </el-table-column>
+      <el-table-column label="分支名称" align="center">
+        <template slot-scope="scope">{{ scope.row.component }}</template>
+      </el-table-column>
+      <el-table-column label="菜单地址" align="center">
+        <template slot-scope="scope">{{ scope.row.path }}</template>
+      </el-table-column>
+      <el-table-column label="类型" align="center">
+        <template slot-scope="scope">
+          {{ scope.row.type==='0' ? '按钮': '菜单' }}</template>
+      </el-table-column>
+      <el-table-column label="操作" width="180" align="center">
         <template slot-scope="scope" type="scope.row">
           <el-tooltip class="item" effect="dark" content="修改" placement="top">
-            <el-button v-ltx="'resourceUpdate'" size="small" type="primary" icon="el-icon-edit" circle @click="action(scope.row)"/>
+            <el-button v-ltx="'resourceUpdate'" type="primary" icon="el-icon-edit" circle @click="action(scope.row)"/>
           </el-tooltip>
           <el-tooltip class="item" effect="dark" content="新增" placement="top">
-            <el-button v-ltx="'resourceAdd'" size="small" :disabled="scope.row.type==='button' ? true:false" type="primary" icon="el-icon-plus" circle @click="action(scope.row,true)"/>
+            <el-button v-ltx="'resourceAdd'" :disabled="scope.row.type==='button' ? true:false" type="primary" icon="el-icon-plus" circle @click="action(scope.row,true)"/>
           </el-tooltip>
           <el-tooltip class="item" effect="dark" content="删除" placement="top">
-            <el-button v-ltx="'resourceDelete'" size="small" :disabled="scope.row.children.length>0?true:false" type="danger" icon="el-icon-delete" circle @click="Delete(scope.row)"/>
+            <el-button v-ltx="'resourceDelete'" :disabled="scope.row.children.length>0?true:false" type="danger" icon="el-icon-delete" circle @click="Delete(scope.row)"/>
           </el-tooltip>
         </template>
       </el-table-column>
-    </el-table>
+    </tree-table>
 
     <!-- 新增/修改弹框 -->
     <el-dialog :title="newTitle" :visible.sync="dialogFormVisible" :before-close="closeResourceDialog" :append-to-body="true" :lock-scroll="false" class="dialogBox">
@@ -118,202 +125,202 @@
 
 </template>
 <script>
-import treeTable from '@/components/TreeTable'
-import api from '@/api/resource.js'
-export default {
-  name: 'TreeTable',
-  components: {
-    treeTable
-  },
-  data() {
-    // 判断是否为正整数
-    const checkFunctionLevel = (rule, value, callback) => {
-      if (!value) {
-        return callback(new Error('菜单排序不能为空'))
-      } else {
-        if (Number.isInteger(Number(value)) && Number(value) > 0 && Number(value) < 999) {
-          callback()
+  import treeTable from '@/components/TreeTable'
+  import api from '@/api/resource.js'
+  export default {
+    name: 'TreeTable',
+    components: {
+      treeTable
+    },
+    data() {
+      // 判断是否为正整数
+      const checkFunctionLevel = (rule, value, callback) => {
+        if (!value) {
+          return callback(new Error('菜单排序不能为空'))
         } else {
-          return callback(new Error('请输入正整数'))
+          if (Number.isInteger(Number(value)) && Number(value) > 0 && Number(value) < 999) {
+            callback()
+          } else {
+            return callback(new Error('请输入正整数'))
+          }
         }
       }
-    }
-    return {
-      newTitle: '',
-      dialogFormVisible: false,
-      menuList: [],
-      flag: true,
-      mark: 1,
-      swithTypeTxt: '',
-      swithMarkTxt: '',
-      // 新增修改时发生
+      return {
+        newTitle: '',
+        dialogFormVisible: false,
+        menuList: [],
+        flag: true,
+        mark: 1,
+        swithTypeTxt: '',
+        swithMarkTxt: '',
+        // 新增修改时发生
+        ruleForm: {
+          name: '', // 菜单名称
+          pId: '', // 父菜单id
+          title: '', // 菜单标题
+          icon: '', // icon图标
+          component: '', // 组件
+          path: '', // 菜单路径
+          isVueFunction: '1', // 设备标识
+          id: '',
+          type: '1',
+          parentName: '',
+          functionLevel: 1 // 菜单排序
+        },
+        rules: {
+          name: [{ required: true, message: '请输入资源名称', trigger: 'blur' }],
+          type: [{ required: true, message: '请选择资源类型', trigger: 'blur' }],
+          ID: [{ required: false, message: '请输入父级资源ID', trigger: 'blur' }],
+          title: [{ required: true, message: '请输入资源标题', trigger: 'blur' }],
+          Mark: [{ required: true, message: '请选择设备类型', trigger: 'blur' }],
+          functionLevel: [{ required: true, message: '请输入菜单排序', validator: checkFunctionLevel, trigger: 'blur' }]
+        }
+      }
+    },
+    watch: {
+      // 监听 type的变化
       ruleForm: {
-        name: '', // 菜单名称
-        pId: '', // 父菜单id
-        title: '', // 菜单标题
-        icon: '', // icon图标
-        component: '', // 组件
-        path: '', // 菜单路径
-        isVueFunction: '1', // 设备标识
-        id: '',
-        type: '1',
-        parentName: '',
-        functionLevel: 1 // 菜单排序
-      },
-      rules: {
-        name: [{ required: true, message: '请输入资源名称', trigger: 'blur' }],
-        type: [{ required: true, message: '请选择资源类型', trigger: 'blur' }],
-        ID: [{ required: false, message: '请输入父级资源ID', trigger: 'blur' }],
-        title: [{ required: true, message: '请输入资源标题', trigger: 'blur' }],
-        Mark: [{ required: true, message: '请选择设备类型', trigger: 'blur' }],
-        functionLevel: [{ required: true, message: '请输入菜单排序', validator: checkFunctionLevel, trigger: 'blur' }]
+        handler(val, oldVal) {
+          // 资源类型
+          switch (val.type) {
+            case '1':
+              this.swithTypeTxt = '菜单'
+              break
+            case '0':
+              this.swithTypeTxt = '按钮'
+              break
+          }
+          // 使用设备
+          switch (val.isVueFunction) {
+            case '1':
+              this.swithMarkTxt = 'PC端'
+              break
+            case '2':
+              this.swithMarkTxt = '移动端'
+              break
+          }
+        },
+        deep: true
       }
-    }
-  },
-  watch: {
-    // 监听 type的变化
-    ruleForm: {
-      handler(val, oldVal) {
-        // 资源类型
-        switch (val.type) {
-          case '1':
-            this.swithTypeTxt = '菜单'
-            break
-          case '0':
-            this.swithTypeTxt = '按钮'
-            break
-        }
-        // 使用设备
-        switch (val.isVueFunction) {
-          case '1':
-            this.swithMarkTxt = 'PC端'
-            break
-          case '2':
-            this.swithMarkTxt = '移动端'
-            break
-        }
-      },
-      deep: true
-    }
-  },
-  created() {
-    this.resourceList(this.ruleForm.isVueFunction)
-  },
-  methods: {
-    action(val, son) {
-      if (val === 'add') {
-        this.newTitle = '新增'
-        this.flag = false
-        this.ruleForm = {}
-      } else if (son) {
-        this.newTitle = val.meta.title + '~新增'
-        this.flag = true
-        this.ruleForm.pId = val.id
-        this.ruleForm.parentName = val.meta.title
-      } else {
-        this.newTitle = '修改'
-        this.flag = true
-        this.getmenuObj(val.id)
-      }
-      this.dialogFormVisible = true
     },
-    // 树形列表
-    resourceList(num) {
-      this.menuList = []
-      api.menuList({ Mark: num, type: 'menu' }).then(res => {
-        this.menuList = res.data.data
-      })
-    },
-    // 删除按钮
-    Delete(data) {
-      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-      }).then(() => {
-        this._menuDelete(data)
-        this.$message({
-          type: 'success',
-          message: '删除成功!'
-        })
-      })
-    },
-    // 删除请求
-    _menuDelete(data) {
-      api.menuDelete(data.id).then(res => {
-        this.resourceList()
-      })
-    },
-    // 关闭dialog
-    closeResourceDialog(data) {
-      this.ruleForm = {}
-      this.flag = false
-      this.newTitle = ''
-      this.dialogFormVisible = false
-    },
-    // 根据id获取单个菜单对象
-    getmenuObj: function(id) {
-      const that = this
-      api.menuCheck(id).then(res => {
-        that.ruleForm = res.data.data
-      })
-    },
-    // 提交对象
-    submitForm(formName) {
-      this.$refs[formName].validate(valid => {
-        if (valid) {
-          console.log(JSON.stringify(this.ruleForm))
-          // 新增
-          api.menuAdd(this.ruleForm).then(res => {
-            this.$emit('comfirm')
-            this.$message({
-              type: 'success',
-              message: '已完成!'
-            })
-            this.$emit('cancel')
-          })
-        } else {
-          return false
-        }
-      })
-      // 刷新当前列表
-      this.closeResourceDialog('')
+    created() {
       this.resourceList(this.ruleForm.isVueFunction)
-      this.dialogFormVisible = false
     },
-    resetForm(formName) {
-      this.$refs[formName].resetFields()
+    methods: {
+      action(val, son) {
+        if (val === 'add') {
+          this.newTitle = '新增'
+          this.flag = false
+          this.ruleForm = {}
+        } else if (son) {
+          this.newTitle = val.meta.title + '~新增'
+          this.flag = true
+          this.ruleForm.pId = val.id
+          this.ruleForm.parentName = val.meta.title
+        } else {
+          this.newTitle = '修改'
+          this.flag = true
+          this.getmenuObj(val.id)
+        }
+        this.dialogFormVisible = true
+      },
+      // 树形列表
+      resourceList(num) {
+        this.menuList = []
+        api.menuList({ Mark: num, type: 'menu' }).then(res => {
+          this.menuList = res.data.data
+        })
+      },
+      // 删除按钮
+      Delete(data) {
+        this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this._menuDelete(data)
+          this.$message({
+            type: 'success',
+            message: '删除成功!'
+          })
+        })
+      },
+      // 删除请求
+      _menuDelete(data) {
+        api.menuDelete(data.id).then(res => {
+          this.resourceList()
+        })
+      },
+      // 关闭dialog
+      closeResourceDialog(data) {
+        this.ruleForm = {}
+        this.flag = false
+        this.newTitle = ''
+        this.dialogFormVisible = false
+      },
+      // 根据id获取单个菜单对象
+      getmenuObj: function(id) {
+        const that = this
+        api.menuCheck(id).then(res => {
+          that.ruleForm = res.data.data
+        })
+      },
+      // 提交对象
+      submitForm(formName) {
+        this.$refs[formName].validate(valid => {
+          if (valid) {
+            console.log(JSON.stringify(this.ruleForm))
+            // 新增
+            api.menuAdd(this.ruleForm).then(res => {
+              this.$emit('comfirm')
+              this.$message({
+                type: 'success',
+                message: '已完成!'
+              })
+              this.$emit('cancel')
+            })
+          } else {
+            return false
+          }
+        })
+        // 刷新当前列表
+        this.closeResourceDialog('')
+        this.resourceList(this.ruleForm.isVueFunction)
+        this.dialogFormVisible = false
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields()
+      }
     }
   }
-}
 </script>
 
 <style lang="scss" scoped>
-.outer-container,
-.content {
-  width: 90vw;
-  height: 65vh;
-}
-.outer-container {
-  position: relative;
-  overflow: hidden;
-}
-.inner-container {
-  position: absolute;
-  left: 0;
-  overflow-y: auto;
-}
+  .outer-container,
+  .content {
+    width: 90vw;
+    height: 65vh;
+  }
+  .outer-container {
+    position: relative;
+    overflow: hidden;
+  }
+  .inner-container {
+    position: absolute;
+    left: 0;
+    overflow-y: auto;
+  }
 
-/* for Chrome */
-.inner-container::-webkit-scrollbar {
-  display: none;
-}
-.btncalss {
-  position: absolute;
-  top: 0.1vw;
-  right: 1vw;
-}
-.pan-btn{
-  font-size: 0.2vw;
-}
+  /* for Chrome */
+  .inner-container::-webkit-scrollbar {
+    display: none;
+  }
+  .btncalss {
+    position: absolute;
+    top: 0.1vw;
+    right: 1vw;
+  }
+  .pan-btn{
+    font-size: 0.2vw;
+  }
 </style>
